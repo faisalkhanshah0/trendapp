02418fe281a8877200060d742ef7f5a9cc6c017d
faisalkhanshah01 <faisalkhanshah@gmail.com>
2018-07-08 17:44:47 +0530

    deployment check

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
  // res.render('index', { title: 'Express' });
- res.json({stauts:1,data:{}});
+ res.json({stauts:1,data:{message:'updated'}});
 });
 router.get('/test', function(req, res, next) {
  // res.render('index', { title: 'Express' });
- res.json({staut:0,data:{}});
+ res.json({staut:0,data:{message:'updated'}});
 });
 module.exports = router;
